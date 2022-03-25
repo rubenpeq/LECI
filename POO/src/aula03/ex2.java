b73b4684d800c789a396065b66e9c057950b9c4f
Ruben <ruben1021@live.com>
2022-03-25 12:11:50 +0000

    Fixed some errors in aulda03

--- POO/src/aula03/ex2.java
+++ POO/src/aula03/ex2.java
@@ -3,13 +3,14 @@ package aula03;
 import java.util.Scanner;
 
 public class ex2 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int N;
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Number? ");
+            N = sc.nextInt();
+        }
         for (N--; N>=0; N--){
             System.out.println(N);
         }
-        sc.close();
     }
 }
--- POO/src/aula03/ex3.java
+++ POO/src/aula03/ex3.java
@@ -2,21 +2,24 @@ package aula03;
 
 import java.util.Scanner;
 
-public static boolean isPrime(int num){
-    if (num <= 1) {
-        return false;
-    }
-    for (int i = 2; i <= Math.sqrt(num); i++) {
-        if (num % i == 0) {
+  public class ex3 {
+    public static boolean isPrime(int num){
+        if (num <= 1) {
             return false;
         }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
-    return true;
-}
 
-  public class ex3 {
-    Scanner sc = new Scanner(System.in);
-    int N = sc.nextInt();
-    System.out.println(N + " is a prime number is a " + isPrime(N) + " statement!");
-    sc.close();
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Number? ");
+        int N = sc.nextInt();
+        System.out.println(N + " is a prime number is a " + isPrime(N) + " statement!");
+        sc.close();
+    }
 }
