b55ccf8a75783b64f063a65ab9151394a4527411
Ruben Pequeno <ruben.pequeno@ua.pt>
2022-03-26 13:47:37 +0000

    POO/aula03 -- done up to ex6

--- POO/src/aula03/ex4.java
+++ POO/src/aula03/ex4.java
@@ -5,22 +5,38 @@ import java.lang.Math;
 
 public class ex4 {
     public static double Max(double n[]){
-        double max=0;
+        double max=n[0];
         for (int i = 1; i<n.length;i++){
-            max=Math.max(n[i-1], n[i]);
+            max=Math.max(max, n[i]);
         }
         return max;
+    }
+    public static double Min(double n[], int c){
+        double min=n[0];
+        for (int i = 1; i<=c;i++){
+            min=Math.min(min, n[i]);
+        }
+        return min;
+    }
+    public static double Average(double n[], int c){
+        double av=0;
+        int i;
+        for (i=0; i<=c;i++){
+            av+=n[i];
+        }
+        return (av/i);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double[] a = new double[64];
-        for (int i=0; i<a.length; i++){
+        int i;
+        for (i=0; i<a.length; i++){
             a[i]=sc.nextDouble();
             if (i>0 && a[i]==a[0]){
                  break;
             }
         }
-        System.out.println(Max(a));
+        System.out.format("Numeros lidos: %d \n Max: %.2f \n Min: %.2f \n Media: %.2f \n", i+1, Max(a), Min(a,i), Average(a, i));
         sc.close();
     }    
 }
---
+++ POO/src/aula03/ex7.java
@@ -0,0 +1,10 @@
+package aula03;
+
+public class ex7 {
+    public static void highLow(){
+        
+    }
+    public static void main(String[] args) {
+    
+    }
+}
