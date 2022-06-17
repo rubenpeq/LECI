7b973f2d633739d278f54618bafeef2add9e54cc
rubenpeq <ruben1021@live.com>
2022-06-17 12:04:08 +0100

    LSD/projeto

--- LSD/projeto/WashingMachine_v3/Display_Decoder.vhd
+++ LSD/projeto/WashingMachine_v3/Display_Decoder.vhd
@@ -2,14 +2,46 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 
 entity DisplayDecoder is
-port(	input : 	in std_logic_vector(3 downto 0);
-		output : 	out std_logic_vector(6 downto 0));
+port(	fsm_mode : 	in std_logic_vector(1 downto 0);
+		real_time :	in std_logic_vector(7 downto 0);
+		p_out : 		out std_logic_vector(6 downto 0);
+		mode_out :	out std_logic_vector(6 downto 0);
+		time0 : 		out std_logic_vector(6 downto 0);
+		time1 : 		out std_logic_vector(6 downto 0));
 end DisplayDecoder;
 
 architecture Behavioral of DisplayDecoder is
 begin
-	output <= 
-		"1110011" when (input = "0001") else --p
-		"1110011" when (input = "0010") else --p1
+	p_out <= "0001100"; -- p
+	
+	mode_out <= 
+		"1111001" when (fsm_mode = "01") else --1
+		"0100100" when (fsm_mode = "10") else --2
+		"0110000" when (fsm_mode = "11") else --3
+		"1110111";
+		
+	time0 <= 
+		"1111001" when (real_time(7 downto 4) = "0001") else --1
+		"0100100" when (real_time(7 downto 4) = "0010") else --2
+		"0110000" when (real_time(7 downto 4) = "0011") else --3
+		"0011001" when (real_time(7 downto 4) = "0100") else --4
+		"0010010" when (real_time(7 downto 4) = "0101") else --5
+		"0000010" when (real_time(7 downto 4) = "0110") else --6
+		"1111000" when (real_time(7 downto 4) = "0111") else --7
+		"0000000" when (real_time(7 downto 4) = "1000") else --8
+		"0010000" when (real_time(7 downto 4) = "1001") else --9
 		"1000000"; --0
+	
+	time1 <= 
+		"1111001" when (real_time(3 downto 0) = "0001") else --1
+		"0100100" when (real_time(3 downto 0) = "0010") else --2
+		"0110000" when (real_time(3 downto 0) = "0011") else --3
+		"0011001" when (real_time(3 downto 0) = "0100") else --4
+		"0010010" when (real_time(3 downto 0) = "0101") else --5
+		"0000010" when (real_time(3 downto 0) = "0110") else --6
+		"1111000" when (real_time(3 downto 0) = "0111") else --7
+		"0000000" when (real_time(3 downto 0) = "1000") else --8
+		"0010000" when (real_time(3 downto 0) = "1001") else --9
+		"1000000"; --0
+		
 end Behavioral;
--- LSD/projeto/WashingMachine_v3/WashMachineFSM.vhd
+++ LSD/projeto/WashingMachine_v3/WashMachineFSM.vhd
@@ -7,7 +7,7 @@ entity WashMachineFSM is
 			P_in:				in std_logic_vector(1 downto 0);
 			start_stop:		in std_logic;
 			time_exp:		in std_logic;
-			door:				in std_logic;	-- '0' porta fechada
+			lid:				in std_logic;	-- '0' porta fechada
 			new_time:			out std_logic;
 			time_value:			out std_logic_vector(2 downto 0);
 			time_enable:		out std_logic;
@@ -20,14 +20,15 @@ entity WashMachineFSM is
 end WashMachineFSM;
 
 architecture Behavioral of WashMachineFSM is
-	constant METER_AGUA_TIME : std_logic_vector(7 downto 0) := ""; -- 5 s
-	constant ENXAGUAR_TIME : std_logic_vector(7 downto 0) := ""; -- 9 s
-	constant TIRAR_AGUA_TIME : std_logic_vector(7 downto 0) := ""; -- 2 s
+	constant SOAK_TIME : std_logic_vector(7 downto 0) := ""; -- 5 s
+	constant RINSE_TIME : std_logic_vector(7 downto 0) := ""; -- 9 s
+	constant REMOVE_WATER_TIME : std_logic_vector(7 downto 0) := ""; -- 2 s
 	constant SPIN_TIME : std_logic_vector(7 downto 0) := ""; -- 4 s
-	type TState is (Tinit, Tp1, Tp2, Tp3, Tp1_ma, Tp1_e, Tp1_ra, Tmet_agua, Tenx, Trem_agua, Tspin, Tend);
+	type TState is (Tinit, Tp1, Tp2, Tp3, Tsoak, Trinse, Trm_water, Tspin, Tfinished);
 	
 	signal s_currentState, s_nextState : TState;
 	signal s_stateChanged : std_logic := '1';
+	signal s_mode : std_logic_vector(1 downto 0);
 
 
 begin
@@ -37,16 +38,10 @@ begin
 			if (reset = '1') then
 				s_currentState <= Tinit;
 			else
-				if (s_currentState /= s_nextState) then
-				s_stateChanged <= '1';
-				else
-				s_stateChanged <= '0';
-				end if;
 			s_currentState <= s_nextState;
 			end if;
 		end if;
 	end process;
-	new_time <= s_stateChanged;
 
 
 	comb_proc : process(s_currentState, p_in)
@@ -54,18 +49,6 @@ begin
 		case (s_currentState) is
 		
 		when  Tinit =>
-			display_out <= "";
-			if (p_in = "01") then
-				s_nextState <= Tp1;
-			elsif (p_in = "10") then
-				s_nextState <= Tp2;
-			elsif (p_in = "11") then
-				s_nextState <= Tp3;
-			else
-				s_nextState <= Tinit;
-			end if;
-			
-		when Tp1 => -- display p1
 			time_value <= x"0";
 			time_enable <= '0';
 			display_out <= "";
@@ -74,42 +57,13 @@ begin
 			rinse <= '0';
 			water_pump <= '0';
 			spin <= '0';
-			if (p_in = "01") then
-				if ((start_stop = '1') and (door = '0')) then
-					s_nextState <= Tp1_ma;
-				end if;
-			else
-				s_nextState <= Tinit;
-			end if;
-		
-		when Tp2 => -- display p2
-			time_value <= x"0";
-			time_enable <= '0';
-			display_out <= "";
-			program_led <= '0';
-			water_valve <= '0';
-			rinse <= '0';
-			water_pump <= '0';
-			spin <= '0';
-			if (p_in = "10") then
-				if ((start_stop = '1') and (door = '0')) then
-					s_nextState <= Tp1_ma;
-				end if;
-			else
-				s_nextState <= Tinit;
-			end if;
-		
-		when Tp3 => -- diplay p3
-			time_value <= x"0";
-			time_enable <= '0';
-			display_out <= "";
-			program_led <= '0';
-			water_valve <= '0';
-			rinse <= '0';
-			water_pump <= '0';
-			spin <= '0';
-			if (p_in = "11") then
-				if ((start_stop = '1') and (door = '0')) then
+			s_mode <= p_in;
+			if (lid = '0') then
+				if ((s_mode = "01") and (start_stop = '1')) then
+					s_nextState <= Tp1;
+				elsif ((s_mode = "10") and (start_stop = '1')) then
+					s_nextState <= Tp2;
+				elsif ((s_mode = "11") and (start_stop = '1')) then
 					s_nextState <= Tspin;
 				end if;
 			else
@@ -126,13 +80,13 @@ begin
 			water_pump <= '0';
 			spin <= '1';
 			
-		when Tend => -- esperar 2s e apagar led
+		when Tfinished => -- esperar 2s e apagar led
 			program_led <= '1';
-			time_value <= TIRAR_AGUA_TIME;
+			time_value <= REMOVE_WATER_TIME;
 			if (time_exp = '1') then
 				s_nextState <= Tinit;
 			else
-				s_nextState <= Tend;
+				s_nextState <= Tfinished;
 			end if;
 		
 		end case;
