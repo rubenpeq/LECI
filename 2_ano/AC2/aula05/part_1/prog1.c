c728b03d3dcf07ec30bb3b35430bfb401b41cff3
rubenpeq <ruben1021@live.com>
2024-04-10 09:01:56 +0100

    AC2 updated 7 segment display codes

--- 2_ano/AC2/aula05/part_1/prog1.c
+++ 2_ano/AC2/aula05/part_1/prog1.c
@@ -4,7 +4,7 @@
 
 void send2displays(unsigned char value)
 {
-    static const char display7Scodes[] = {0x3f, 0x06, 0x5b, 0x4f};
+    static const char display7Scodes[] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x67, 0x77, 0x7c, 0x39, 0x5e, 0x79, 0x71};
     static char displayFlag = 0; // static variable: doesn't loose its value between calls to function
     int digit_low, digit_high;
     digit_high = (value >> 4);    // send digit_high (dh) to display: dh = value >> 4
