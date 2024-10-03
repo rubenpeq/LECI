07617208fbd5a9da709affce74cbc578af030095
rubenpeq <ruben1021@live.com>
2024-10-03 23:22:21 +0100

    AC1 aula02
    ex2 completed with some extra printing code

--- 2_ano/AC1/aula02/ex3d.asm
+++ 2_ano/AC1/aula02/ex3d.asm
@@ -1,18 +1,18 @@
     # AC1/aula02/ex3d.asm
-    
-	.data
-str1: 	.asciiz "Introduza 2 numeros\n"
-str2: 	.asciiz "A soma dos dois numeros é: "
-	.eqv print_string, 4
-	.eqv read_int, 5
-	.eqv print_int10, 1
-	.text
-	.globl main
-main: 	la $a0,str1
-	ori $v0,$0,print_string
-	syscall # print_string(str1)
-	ori $v0, $0, read_int
-	syscall # $v0 = read_int()
-	or $t0,$v0,$0 # $t0 = read_int()
-	(...)
-	jr $ra # end program
+
+		.data
+str1:   .asciiz "Introduza 2 numeros\n"
+str2:   .asciiz "A soma dos dois numeros é: "
+        .eqv    print_string, 4
+        .eqv    read_int, 5
+        .eqv    print_int10, 1
+		.text
+        .globl  main
+main:       la      $a0,    str1
+    ori     $v0,    $0,     print_string
+    syscall                                     # print_string(str1)
+    ori     $v0,    $0,     read_int
+    syscall                                     # $v0 = read_int()
+    or      $t0,    $v0,    $0                  # $t0 = read_int()
+    (...)
+    jr      $ra                                 # end program
