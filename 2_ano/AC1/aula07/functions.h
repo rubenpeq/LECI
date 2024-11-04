1b6cfadb232bcd25b7a66cf9b4fc7026ab75c81c
rubenpeq <ruben1021@live.com>
2024-11-04 22:58:22 +0000

    AC1 aula07
    .c programs

---
+++ 2_ano/AC1/aula07/functions.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+int strlen(char *s);
+void exchange(char *c1, char *c2);
+char *strrev(char *str);
+char *strcpy(char *dst, char *src);
+char *strcpy2(char *dst, char *src);
+char *strcat(char *dst, char *src);
+
+#endif
